bab3a91437a0b6ffebf29d9bb96338de717f8756
William <williamw15@protonmail.com>
2022-11-24 15:42:12 -0500

    add logged in screen with test text

---
+++ screens/LoggedIn/LoggedIn.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+
+export default function LoggedIn() {
+
+    return (
+        <View>
+            <Text>Logged in works!</Text>
+        </View>
+    )
+}
