376dad6d7f60fbfebf218393cf1f014292522172
William <williamw15@protonmail.com>
2022-11-25 12:29:53 -0500

    map returns selected object, need to get object deconstructed and displayed in Modal

--- screens/gallerylist/GalleryList.js
+++ screens/gallerylist/GalleryList.js
@@ -1,5 +1,15 @@
+/** @format */
+
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, SafeAreaView, Pressable, Modal, ImageBackground } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Pressable,
+  Modal,
+  ImageBackground,
+} from 'react-native';
 
 import Button from '../../components/Button';
 import { ScrollView } from 'react-native-gesture-handler';
@@ -10,79 +20,119 @@ import { Background } from '../../Background/Background';
 const constellationData = require('../../constellation.json');
 
 export default function GalleryList() {
-    const [ detailedInformation, setDetailedInformation ] = useState(false);
+  const [modalDisplay, setModalDisplay] = useState(false);
+  const [selectedConstellationData, setSelectedConstellationData] = useState();
 
-    return (
-        <View>
-            <Modal visible={ detailedInformation } animationType='fade'>
+  //? event handler may not be required for this, useState may be enough
+  onSelectedConstellationDataHandler = (value) => {
+    setSelectedConstellationData(value);
+    console.log(value);
+  };
+
+  return (
+    <View>
+      {/* <Modal visible={ modalDisplay } animationType='fade'>
             <ImageBackground style={ { flex: 1 } } blurRadius={ 1 } source={ { uri: Background } }>
                 <View>
                     <MaterialIcons
                     name='close'
                     size={ 35 }
-                    onPress={ () => { setDetailedInformation(false) } }
+                    onPress={ () => { setModalDisplay(false) } }
                     style={ styles.modalToggle } />
+                    <Text>{ item.name }</Text>
                 </View>
                 </ImageBackground>
+            </Modal> */}
+      <SafeAreaView>
+        <ScrollView>
+          <View style={styles.container}>
+            {constellationData.map((item, key) => (
+              <LinearGradient
+                colors={['rgba(191, 0, 255, .2)', 'rgba(115, 0, 153, .2)', 'rgba(0, 64, 128, .2)']}
+                style={styles.gradient}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 1, y: 1 }}
+              >
+                <Pressable
+                  style={styles.pressableContainer}
+                  key={constellationData[key].id}
+                  onPress={() => {
+                    setModalDisplay(true);
+                    onSelectedConstellationDataHandler(constellationData[key]);
+                    // console.log(constellationData[key]);
+                  }}
+                >
+                  <Text style={styles.textFormat}>{item.name}</Text>
+                </Pressable>
+              </LinearGradient>
+            ))}
+            <Modal
+              visible={modalDisplay}
+              animationType='fade'
+            >
+              <ImageBackground
+                style={{ flex: 1 }}
+                blurRadius={1}
+                source={{ uri: Background }}
+              >
+                <View>
+                  <MaterialIcons
+                    name='close'
+                    size={35}
+                    onPress={() => {
+                      setModalDisplay(false);
+                    }}
+                    style={styles.modalToggle}
+                  />
+                  //! second map not required since only returning 1 object //TODO look up object
+                  //TODO deconstruction for values
+                  {/* <Text>{ selectedConstellationData.id }</Text> */}
+                  {
+                    // selectedConstellationData.map((item, key) => {
+                    // <Text>{ item[key].id }</Text>
+                    // })
+                  }
+                </View>
+              </ImageBackground>
             </Modal>
-            <SafeAreaView>
-                <ScrollView>
-                    <View style={ styles.container }>
-                        {
-                            constellationData.map((item, key) => (
-                                <LinearGradient
-                                    colors={ [
-                                        'rgba(0, 64, 128, .2)',
-                                        'rgba(115, 0, 153, .2)',
-                                        'rgba(191, 0, 255, .2)' ] }
-                                    style={ styles.gradient }
-                                    start={ { x: 0, y: 0 } }
-                                    end={ { x: 1, y: 1 } }
-                                >
-                                    <Pressable style={ styles.pressableContainer } key={ constellationData[ key ].id } onPress={ () => { setDetailedInformation(true) } }>
-                                        <Text style={ styles.textFormat }>{ item.name }</Text>
-                                    </Pressable>
-                                </LinearGradient>
-                            ))
-                        }
-                    </View>
-                </ScrollView>
-            </SafeAreaView>
-        </View>
-    )
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    textFormat: {
-        color: '#aa80ff',
-        fontSize: 24,
-    },
-    container: {
-        alignItems: 'center',
-        flex: 1,
-        marginTop: 50
-    },
-    pressableContainer: {
-        alignItems: 'center',
-        paddingTop: 5,
-        paddingBottom: 5,
-        width: 300,
-        marginTop: 10,
-        marginBottom: 10,
-
-    },
-    gradient: {
-        height: 60,
-        width: 300,
-        marginBottom: 20,
-    },
-    modalToggle: {
-        marginBottom: 20,
-        color: '#aa80ff',
-        borderWidth: 1,
-        borderColor: 'rgb(191, 0, 255)',
-        textAlign: 'right',
-        marginLeft: 345,
-        marginRight: 30
-    }
-})
+  textFormat: {
+    color: '#aa80ff',
+    fontSize: 24,
+  },
+  container: {
+    alignItems: 'center',
+    flex: 1,
+    marginTop: 50,
+  },
+  pressableContainer: {
+    alignItems: 'center',
+    paddingTop: 5,
+    paddingBottom: 5,
+    width: 300,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  gradient: {
+    height: 60,
+    width: 300,
+    marginBottom: 20,
+  },
+  modalToggle: {
+    marginBottom: 20,
+    color: 'white',
+    borderWidth: 1,
+    borderColor: 'white',
+    textAlign: 'right',
+    marginLeft: 345,
+    marginRight: 30,
+    //rgb(191, 0, 255)
+  },
+});
