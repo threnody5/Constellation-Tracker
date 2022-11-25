66b2476ef2dd121b0e586927ede01319325ec269
William <williamw15@protonmail.com>
2022-11-24 21:47:17 -0500

    add linear gradient to button styling with white border

--- components/Button.js
+++ components/Button.js
@@ -1,22 +1,48 @@
 import { Text, Pressable, StyleSheet, View } from 'react-native';
 
+import { LinearGradient } from 'expo-linear-gradient';
+
 export default function Button(props) {
-    const { onPress, title, style } = props;
+    const { onPress, title } = props;
 
     return (
-        <View>
-            <Pressable style={ style } onPress={ onPress }>
-                <Text style={ styles.textDecoration }>{ title }</Text>
-            </Pressable>
+        <View style={ styles.container }>
+            <LinearGradient
+                colors={ [
+                    'rgba(0, 64, 128, .4)',
+                    'rgba(115, 0, 153, .4)',
+                    'rgba(191, 0, 255, .4)' ] }
+                style={ styles.gradient }
+                start={ { x: 0, y: 0 } }
+                end={ { x: 1, y: 1 } }
+            >
+                <Pressable style={ styles.buttonContainer } onPress={ onPress }>
+                    <Text style={ styles.textDecoration }>{ title }</Text>
+                </Pressable>
+            </LinearGradient>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     textDecoration: {
-        color: 'black',
+        color: 'white',
         textAlign: 'center',
         fontSize: 20,
         textTransform: 'uppercase',
+        paddingTop: 21,
+        paddingBottom: 23
+    },
+    buttonContainer: {
+        borderWidth: 1,
+        borderColor: 'white',
+    },
+    gradient: {
+        height: 70,
+        width: 300,
+        marginBottom: 25
+    },
+    container: {
+        alignItems: 'center',
     }
 })
